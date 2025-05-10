1ad72aad6fbf5dd57707089810819a4eabbd17cd
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-10 21:11:15 +0300

    Create simple_calculator.cpp
    not full version

---
+++ simple_calculator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+int main() {
+  cout << "Heyo! Maybe you want solve something? Y/n" << endl;
+  cin << string answer{};
+
+  switch(answer) {
+    case 1:
+      if (answer = y) {
+        cout << "Super!" << '\n';
+      };
+    case 2:
+      if (answer = n) {
+        cout << "Sadness. << '\n';
+          break;
+      };
+
+
+
+
