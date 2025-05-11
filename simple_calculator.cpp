05971427bd614240b04b67632806f066786ca1d7
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-11 16:55:55 +0300

    Update simple_calculator.cpp
    broken tabs fixed

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -3,44 +3,44 @@
 int main() {
   std::cout << "Heyo! Maybe you want solve something? Y/n" << std::endl;
   char a;
-	std::cin >> a; // answer
+    std::cin >> a; // answer
 
   switch(a) { // request a response from the user to continue the program
     case 'y':
       std::cout << "Super!" << '\n';
       break;
 
-		case 'n':
+    case 'n':
       std::cout << "Sadness." << '\n';
       break;
 
-		std::cout << "Try again!" << std::endl;
-    return 0;
-    };
+  std::cout << "Try again!" << std::endl;
+  return 0;
+  };
 	
   double x, y;
-  std::cout << "Write first number please: " << '\n';
+  std::cout << "Write first number please: ";
   std::cin >> x;
-  std::cout << "Write second number please: " << '\n';
+  std::cout << "Write second number please: ";
   std::cin >> y;
 
-  std::cout << "What you want? +, -, *, / ?" << '\n';
+  std::cout << "What you want? +, -, *, / ? ";
   char s;
-	std::cin >> s; // symbol
+  std::cin >> s; // symbol
   switch(s) {
     case '+':
       std::cout <<"Result: " << x + y << std::endl;
       break;
 		
-		case '-':
+    case '-':
       std::cout << "Result: " << x - y << std::endl;
       break;
 		
-		case '*':
+    case '*':
       std::cout << "Result: " << x * y << std::endl;
       break;
 		
-		case '/':
+    case '/':
       std::cout << "Result: " << x / y << std::endl;
       break;
   };
