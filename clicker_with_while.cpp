a46b623a5b6fada1b0751a70c39f2ea6ed973c10
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-12 20:49:50 +0300

    Create clicker_with_while.cpp
    this code doesnt work, fix soon!

---
+++ clicker_with_while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::cout << "Heyo! Here Miyo Kislyy!" << std::endl;
+	std::cout << "Do you want play in clicker? Y/n ";
+	char a; // answer
+	std::cin >> a;
+
+	while(a = 'y') {
+		int x = 0;
+		char symbol;
+		if(symbol == ' ') {
+		++x;
+		std::cout << x;
+		};
+	};
+}
