537fe1f228045e61f83d4a832041ec81bc257287
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-13 22:42:45 +0300

    Create arifmetic_progression.cpp
    this version work badly, but fix soon

---
+++ arifmetic_progression.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main() {
+	int a, n, b, s; // firts penis, quantity of penis, diff and sum
+	char answer;
+  std::cout << "Heyo! Here Miyo!\n";
+	std::cout << "Do you want solve? (Y/n): ";
+	std::cin >> answer;
+  while(answer == 'y') {
+		std::cout << "Please? Write what you want:\n1. find sum\n2. find first penis\n3. find difference\n4. find quantity of penis\n: ";
+		int choice;
+		double result;
+		std::cin >> choice;
+	  switch(choice) {
+		  case 1:
+				std::cout << "\nWrite a, b and n: ";
+				std::cin >> a, b, n;
+			  result = a + b*(n-1); // sum
+
+		  case 2:
+				std::cout << "\nWrite s, b and n: ";
+				std::cin >> s, b, n;
+			  result = s - b*(n-1); // first penis
+
+  		case 3:
+				std::cout << "\nWrite s, a and n: ";
+				std::cin >> s, a, n;
+	  		result = s / (a * n); // diff
+
+		  case 4:
+				std::cout << "\nWrite s, a and b: ";
+				std::cin >> s, a, b;
+			  result = (s - a) / b; // quantity
+		};
+		std::cout << "Result: ";
+		return result;
+	};
+	std::cout << "Bye!" << std::endl;
+	return 0;
+};
+
