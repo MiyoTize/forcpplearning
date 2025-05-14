a2f3167b62fad41e49d79e031cff2fd2fccf11a2
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-14 16:02:25 +0300

    Update simple_2dmatrix.cpp
    tabs broken...

--- simple_2dmatrix.cpp
+++ simple_2dmatrix.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int main() {
-	int matrix[3][3] = {
+  int matrix[3][3] = {
   {1, 2, 3},
-	{1, 2, 3},
-	{1, 2, 3}
-	};
+  {1, 2, 3},
+  {1, 2, 3}
+  };
   
   int x, y;
-	std::cout << "Please, write x and y of matrix: ";
-	std::cin >> x, y;
-	std::cout << "\nResult: " << matrix[x][y];
+  std::cout << "Please, write x and y of matrix: ";
+  std::cin >> x, y;
+  std::cout << "\nResult: " << matrix[x][y];
 };
 	
