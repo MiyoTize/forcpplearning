321ae27550422fb0357904ad168a77e02980ada2
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-15 23:54:39 +0300

    Update simple_2dmatrix.cpp
    tabs fixed

--- simple_2dmatrix.cpp
+++ simple_2dmatrix.cpp
@@ -12,13 +12,13 @@ int main() {
   std::cin >> answer;
   switch(answer) {
     case 1:
-  int x = 0, y = 0;
-  while(matrix[x] > 0) {
-    while(matrix[y] >0) {
-      ++y;
-      std::cout << matrix[x][y];
+      int x = 0, y = 0;
+      while(matrix[x] > 0) {
+        while(matrix[y] >0) {
+          ++y;
+          std::cout << matrix[x][y];
+          }
+        ++x;
+        std::cout << matrix[x][y];
       }
-    ++x;
-    std::cout << matrix[x][y];
-  }
 };
