5991f201fb3000c9055fe8029e7cb4fa4e0eac51
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-15 23:53:52 +0300

    Update simple_2dmatrix.cpp

--- simple_2dmatrix.cpp
+++ simple_2dmatrix.cpp
@@ -7,6 +7,11 @@ int main() {
   {1, 2, 3}
   };
   
+  char answer;
+  std::cout << "What do you want? "; 
+  std::cin >> answer;
+  switch(answer) {
+    case 1:
   int x = 0, y = 0;
   while(matrix[x] > 0) {
     while(matrix[y] >0) {
