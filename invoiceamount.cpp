65ea80edd8055d389ea7d1f25a968579c19e86bf
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-16 22:41:10 +0300

    Create invoiceamount.cpp

---
+++ invoiceamount.cpp
@@ -0,0 +1,33 @@
+
+// THIS CODE DOESNT WORK
+
+#include <iostream>
+
+int sum(double, double, double, double, double)
+{
+  std::cout << "Please, write the number :";
+  double num{};
+	std::cin >> num;
+	std::cout << " ";
+	return num;
+}
+
+int main() {
+  std::cout << "Heyo! Here Miyo!\n";
+	std::cout << "Please, write numbers for sentences: ";
+	double x{ sum() };
+	double y{ sum() };
+	double z{ sum() };
+	double a{ sum() };
+	double b{ sum() };
+
+	std::cout << "Right? Numbers: " << x << " " << y << " " << z << " " << a << " " << b << " (Y/n): ";
+	char answer;
+	if(answer == 'y') {
+	  std::cout << "Result: " << x + y + z + a + b;
+	}
+	else {
+	std::cout << "Ooooops... Try again, please";
+	return 0;
+  }
+}
