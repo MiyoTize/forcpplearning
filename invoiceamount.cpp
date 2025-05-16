2ff251ea657733c66e04140272fb7209fbf7935e
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-16 22:42:26 +0300

    Update invoiceamount.cpp
    tabs fixed

--- invoiceamount.cpp
+++ invoiceamount.cpp
@@ -7,27 +7,27 @@ int sum(double, double, double, double, double)
 {
   std::cout << "Please, write the number :";
   double num{};
-	std::cin >> num;
-	std::cout << " ";
-	return num;
+  std::cin >> num;
+  std::cout << " ";
+  return num;
 }
 
 int main() {
   std::cout << "Heyo! Here Miyo!\n";
-	std::cout << "Please, write numbers for sentences: ";
-	double x{ sum() };
-	double y{ sum() };
-	double z{ sum() };
-	double a{ sum() };
-	double b{ sum() };
+  std::cout << "Please, write numbers for sentences: ";
+  double x{ sum() };
+  double y{ sum() };
+  double z{ sum() };
+  double a{ sum() };
+  double b{ sum() };
 
-	std::cout << "Right? Numbers: " << x << " " << y << " " << z << " " << a << " " << b << " (Y/n): ";
-	char answer;
-	if(answer == 'y') {
-	  std::cout << "Result: " << x + y + z + a + b;
-	}
-	else {
-	std::cout << "Ooooops... Try again, please";
-	return 0;
+  std::cout << "Right? Numbers: " << x << " " << y << " " << z << " " << a << " " << b << " (Y/n): ";
+  char answer;
+  if(answer == 'y') {
+    std::cout << "Result: " << x + y + z + a + b;
+  }
+  else {
+  std::cout << "Ooooops... Try again, please";
+  return 0;
   }
 }
