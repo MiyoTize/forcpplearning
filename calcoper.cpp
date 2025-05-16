8985cd4bc716b4c2da7594f997e54d10df5797fb
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-16 18:33:41 +0300

    Update calcoper.cpp
    tabs fixed

--- calcoper.cpp
+++ calcoper.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Heyo! Here Miyo!\n";
-	std::cout << "Do you want solve something? (Y/n): ";
+  std::cout << "Heyo! Here Miyo!\n";
+  std::cout << "Do you want solve something? (Y/n): ";
   char answer; // y or n from user
-	std::cin >> answer;
+  std::cin >> answer;
   
-	std::cout << "\nHow many times You want use my code?\n";
-	std::cout << "Please, write here: ";
-	int n, z = 0; // n - how many times, z - strange way, but
-	std::cin >> n;
+  std::cout << "\nHow many times You want use my code?\n";
+  std::cout << "Please, write here: ";
+  int n, z = 0; // n - how many times, z - strange way, but
+  std::cin >> n;
 
-	while(z!=n and n>0) {
-		if(answer == 'y') {
-	    double solve(double x, double y) {
-        std::cout << "\nPlease, write here x and y:";
-			  std::cin >> x, y;
+  while(z!=n and n>0) {
+    if(answer == 'y') {
+      double solve(double x, double y) {
+      std::cout << "\nPlease, write here x and y:";
+      std::cin >> x, y;
 
-			  std::cout << "\nResult(+): " << z{x+y};
-    	  std::cout << "\nResult(-): " << z{x-y};
-      	std::cout << "\nResult(*): " << z{x*y};
-        std::cout << "\nResult(/): " << z{x/y} << "\n";
-		  }
-		}
-		else {
+      std::cout << "\nResult(+): " << z{x+y};
+      std::cout << "\nResult(-): " << z{x-y};
+      std::cout << "\nResult(*): " << z{x*y};
+      std::cout << "\nResult(/): " << z{x/y} << "\n";
+      }
+    }
+    else {
     break;
-		}
-		++z;
-	}
-	std::cout << "Bye!" << std::endl;
-	return 0;
+    }
+    ++z;
+  }
+  std::cout << "Bye!" << std::endl;
+  return 0;
 }
