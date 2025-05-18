d829be65b236f2805d9b3f40d8b46276816c985b
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-18 23:34:29 +0300

    Update clicker_with_while.cpp
    tabs fixed

--- clicker_with_while.cpp
+++ clicker_with_while.cpp
@@ -4,20 +4,20 @@
 #include <iostream>
 
 int click() {
-	std::cout << x << " + 1 = ";
-	++x;
-	std::cout << x << "\n";
+  std::cout << x << " + 1 = ";
+  ++x;
+  std::cout << x << "\n";
   return 0;
 }
 
 int main() {
-	int x = 0;
+  int x = 0;
   std::cout << "Heyo! Here Miyo Kislyy!";
   std::cout << "Do you want play in clicker? You need click 1000 times. (Y/n): ";
   char a; // answer
   std::cin >> a;
   
-	if(a == 'y') {
+  if(a == 'y') {
     while(x <= 1000) {
       int z = click();
       ++x;
