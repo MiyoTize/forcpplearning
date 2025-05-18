ad355c7323f418842484373047e295c10bef942b
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-18 23:14:06 +0300

    Update clicker_with_while.cpp
    tabs, comment and return 0

--- clicker_with_while.cpp
+++ clicker_with_while.cpp
@@ -1,18 +1,22 @@
+
+// THIS CRAZY CODE DOESNT WORK. FIX SOON
+
 #include <iostream>
 #include <string>
 
 int main() {
-	std::cout << "Heyo! Here Miyo Kislyy!";
-	std::cout << "Do you want play in clicker? Y/n ";
-	char a; // answer
-	std::cin >> a;
+  std::cout << "Heyo! Here Miyo Kislyy!";
+  std::cout << "Do you want play in clicker? Y/n ";
+  char a; // answer
+  std::cin >> a;
 
-	while(a = 'y') {
-		int x = 0;
-		char symbol;
-		if(symbol == ' ') {
-		++x;
-		std::cout << x;
-		};
-	};
+  while(a = 'y') {
+    int x = 0;
+    char symbol;
+    if(symbol == ' ') {
+      ++x;
+      std::cout << x;
+    }
+  }
+  return 0;
 }
