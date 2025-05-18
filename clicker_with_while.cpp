2d89ff485e42b6c740086e3e1fd31ce18616af5c
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-18 23:49:47 +0300

    Update clicker_with_while.cpp

--- clicker_with_while.cpp
+++ clicker_with_while.cpp
@@ -4,24 +4,29 @@
 #include <iostream>
 
 int click() {
-  std::cout << x << " + 1 = ";
-  ++x;
-  std::cout << x << "\n";
+	int x;
+	char y;
+	std::cout << x << " + 1 = ";
+	std::cin >> y;
+	if(y == ' ') {
+	  ++x;
+	  std::cout << x << "\n";
+	}
   return 0;
 }
 
 int main() {
-  int x = 0;
+	int x = 0;
+	char y;
   std::cout << "Heyo! Here Miyo Kislyy!";
   std::cout << "Do you want play in clicker? You need click 1000 times. (Y/n): ";
   char a; // answer
   std::cin >> a;
-  
   if(a == 'y') {
     while(x <= 1000) {
       int z = click();
-      ++x;
     }
-  }
+	}
+	std::cout << "Bye!";
   return 0;
 }
