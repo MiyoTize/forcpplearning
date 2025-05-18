23e956bf55ea7a13e324085ac8c7e098ce8f7069
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-18 23:33:15 +0300

    Update clicker_with_while.cpp
    find x

--- clicker_with_while.cpp
+++ clicker_with_while.cpp
@@ -2,20 +2,25 @@
 // THIS CRAZY CODE DOESNT WORK. FIX SOON
 
 #include <iostream>
-#include <string>
+
+int click() {
+	std::cout << x << " + 1 = ";
+	++x;
+	std::cout << x << "\n";
+  return 0;
+}
 
 int main() {
+	int x = 0;
   std::cout << "Heyo! Here Miyo Kislyy!";
-  std::cout << "Do you want play in clicker? Y/n ";
+  std::cout << "Do you want play in clicker? You need click 1000 times. (Y/n): ";
   char a; // answer
   std::cin >> a;
-
-  while(a = 'y') {
-    int x = 0;
-    char symbol;
-    if(symbol == ' ') {
+  
+	if(a == 'y') {
+    while(x <= 1000) {
+      int z = click();
       ++x;
-      std::cout << x;
     }
   }
   return 0;
