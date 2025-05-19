0df3526cc47fbab0b3cf444265a9413f665ba347
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-20 00:05:43 +0300

    Create sortmassive.cpp

---
+++ sortmassive.cpp
@@ -0,0 +1,8 @@
+#include <algorithm>
+
+int main() {
+  int fooarray[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int array[]   =  {5, 3, 4, 2, 0, 9, 8, 1, 6, 7}; 
+  std::sort(array, array + sizeof(array) / sizeof(int));
+  return 0;
+}
