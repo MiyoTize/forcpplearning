902a963979474ebf580848fd3620b133aeb09076
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-19 23:26:22 +0300

    Update calcoper.cpp
    one moment

--- calcoper.cpp
+++ calcoper.cpp
@@ -8,24 +8,23 @@ int main() {
   std::cout << "Do you want solve something? (Y/n): ";
   char answer; // y or n from user
   std::cin >> answer;
-  
-  std::cout << "\nHow many times You want use my code?\n";
-  std::cout << "Please, write here: ";
-  int n, m, z = 0; // n - how many times, z - strange way, but
-  std::cin >> n;
+}
+
 
   while(z!=n && n>0) {
     if(answer == 'y') {
-      int x;
-      int y;
-      std::cout << "\nPlease, write here x and y:";
-      std::cin >> x, y;
 
-      std::cout << "\nResult(+): " << m{x+y};
-      std::cout << "\nResult(-): " << m{x-y};
-      std::cout << "\nResult(*): " << m{x*y};
-      std::cout << "\nResult(/): " << m{x/y} << "\n";
-    }
+int calc() {
+  int x;
+  int y;
+  std::cout << "\nPlease, write here x and y:";
+  std::cin >> x, y;
+
+  std::cout << "\nResult(+): " << m{x+y};
+  std::cout << "\nResult(-): " << m{x-y};
+  std::cout << "\nResult(*): " << m{x*y};
+  std::cout << "\nResult(/): " << m{x/y} << "\n";
+}
     else {
       break;
     }
