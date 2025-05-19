bf2ad17cc096f2f3df6213e262d8d41ba733117a
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-19 23:58:58 +0300

    Create fibnumbers.cpp

---
+++ fibnumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int fib(int n){
+    int arr[n]{0,1};
+    for(int i=2;i<=n;i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+    return 0;
+}
