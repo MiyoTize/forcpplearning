dce018f72cb131e56c3f42facd3c0a7aacf85829
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-20 00:29:46 +0300

    Update fibnumbers.cpp

--- fibnumbers.cpp
+++ fibnumbers.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 
 int fib(int n) {
-    int arr[n]{0,1};
-    for(int i=2;i<=n;i++) {
-        arr[i] = arr[i-1] + arr[i-2];
-    }
-    return arr[n];
+  int arr[n]{0,1};
+  for(int i=2; i<=n; i++) {
+    arr[i] = arr[i-1] + arr[i-2];
+  }
+  return arr[n];
 }
-int main(){
-    int n;
-    std::cin >> n;
-    std::cout << fib(n) << endl;
-    return 0;
+
+int main() {
+  std::cout << "Heyo! Here Miyo!";
+  std::cout << "Do you want fibnum? (Y/n): ";
+  char a;
+  std::cin >> a; // asnwer
+    
+  int n = 1; // number of fib
+  while(a == 'y') {
+    fib(n);
+    ++n;
+    std::cout << "Do you want continue? (Y/n): "
+    std::cin >> a;
+  }
+  return 0;
 }
