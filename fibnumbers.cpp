303a19081a62e8e84b1f30a7681e974959eec759
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-19 23:59:50 +0300

    Update fibnumbers.cpp

--- fibnumbers.cpp
+++ fibnumbers.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int fib(int n){
+int fib(int n) {
     int arr[n]{0,1};
-    for(int i=2;i<=n;i++){
+    for(int i=2;i<=n;i++) {
         arr[i] = arr[i-1] + arr[i-2];
     }
     return arr[n];
