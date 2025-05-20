74f603608d3e934e504cb84a05323b8e61cf4721
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-21 00:32:50 +0300

    Create NotSimpleCalculator.cpp
    crazy

---
+++ NotSimpleCalculator.cpp
@@ -0,0 +1,28 @@
+
+// ITS DOESNT WORK U SEE?????????
+
+#include <iostream>
+#include <cmath>
+#include <cmake>
+
+int ftext() { // first text
+  char a;
+  std::cout << "Heyo! Here Miyo!\n"
+            << "You in NotSimpleCalculator. Missclick or you want start?\n"
+            << "(Y/n): "
+  std::cin >> a; // answer
+
+}
+
+int stext() { // second text
+  std::cout << "You have a choice:\n"
+<<
+<<
+
+int main() {
+  char a;
+  int z = text();
+    while(a == 'y') {
+    f()
+    std::cin >> a;
+  }
