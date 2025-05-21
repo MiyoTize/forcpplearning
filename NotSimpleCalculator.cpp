cf77baa62e08f3a98fe2c143b15ef72c4065758d
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-21 20:38:51 +0300

    Update NotSimpleCalculator.cpp
    some shitty stuff

--- NotSimpleCalculator.cpp
+++ NotSimpleCalculator.cpp
@@ -5,24 +5,41 @@
 #include <cmath>
 #include <cmake>
 
-int ftext() { // first text
-  char a;
+int ftext(char answer) { // first text
+  char answer;
   std::cout << "Heyo! Here Miyo!\n"
             << "You in NotSimpleCalculator. Missclick or you want start?\n"
-            << "(Y/n): "
-  std::cin >> a; // answer
-
+            << "(Y/n): ";
+  std::cin >> answer; // answer
 }
 
-int stext() { // second text
+int stext(int choice) { // second text
+  int choice; // number of:
   std::cout << "You have a choice:\n"
-<<
-<<
+            << "1. Massives\n"
+            << "2. Matrix ( 1d or 2d )\n"
+            << "3. Based meth\n"
+            << "4. Bitwise operations\n"
+  std::cin >> choice;
+}
+
+
+
+int out() {
+  if ...
+    1
+  else if ...
+    2
+}
 
 int main() {
-  char a;
+  char answer;
   int z = text();
-    while(a == 'y') {
-    f()
-    std::cin >> a;
+  while(a == 'y') {
+    int q = out();
+    std::cout << "Do you want start again? (Y/n): ";
+    std::cin >> answer;
   }
+  std::cout << "\nBye!";
+  return 0;
+}
