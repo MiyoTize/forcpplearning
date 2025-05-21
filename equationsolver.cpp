4a6b2c79655bac65865793d4ad5df8ee26307e1f
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-21 23:29:11 +0300

    Update equationsolver.cpp

--- equationsolver.cpp
+++ equationsolver.cpp
@@ -5,22 +5,23 @@
 #include <cmath>
 
 int first(double a, double b, double &x) {
+  int result;
   if(a == 0) {
-    return 0;
+    result = 0;
   } 
   else {
     x = -b / a;
-    return 1;
+    result = 1;
   }
-  return return;
+  return result;
 }
-
 int operfirst() {
+  int result;
   std::cout << "Please, write A and B: ";
   std::cin >> a;
   std::cin >> b;   
-  int result = first(a, b, x);   
-  if(return == 1) {
+  int endfirst = first(a, b, x);   
+  if(result == 1) {
     std::cout << "\nX = " << x;
   }
   else {
@@ -29,31 +30,54 @@ int operfirst() {
   return 0;
 }
 
-
-
-
-
 int second(double a, double b, double c) {
+  int result;
   double a, b, c, D, x1, x2;
-  D = b * b - (4 * a * c);
-
-  if(a == 0) {
+  D = b * b - (4 * a * c); 
+  if(a == 0) { 
     x1 = -c / b;
-    return 0; // Bx + C = 0
+    result = 0; // Bx + C = 0
   }
   else if(b == 0 && c <= 0) {
     x1 = sqrt(-c);
     x2 = -sqrt(-c);
-    return 1; // Ax^2 + C = 0
-  }
+    result = 1; // Ax^2 + C = 0
+  } 
   else if(c == 0) {
     x1 = 0;
-    x2 = -a
-    return 2; // Ax^2 + B = 0
+    x2 = -b / a;
+    result 2; // Ax^2 + Bx = 0\
+  } 
+  else {
+    if(D < 0) {
+      result = 3; // Dont have x1 or x2
+    }   
+    else if (D == 0) {
+      x1 = -b / (2 * a);
+      result = 4;
+    }
+    else {
+      x1 = (-b + sqrt(D)) / (2 * a);
+      x2 = (-b - sqrt(D)) / (2 * a);
+      result = 5;
+    }
+    return 0;
   }
-  return return;
+  return result;
 }
-
+int opersecond() {
+  std::cout << "Please, write A, B and C: ";
+  std::cin >> a;
+  std::cin >> b;
+  std::cin >> c;
+  int result = second(a, b, c);
+  if(result == 1) {
+    std::cout << "x1 = " << x1 << "\n"
+              << "x2 = " << x2;
+  }
+  return 0;
+}
+  
 int main() {
   double a, b, x; // Ax + B = 0
   int answer; 
@@ -76,15 +100,4 @@ int main() {
 
 
 
-int opersecond() {
-  std::cout << "Please, write A, B and C: ";
-  std::cin >> a;
-  std::cin >> b;
-  std::cin >> c;
 
-  int result = second(a, b, c);
-
-  if(result == 1) {
-    std::cout << "x1 = " << x1 << "\n"
-              << "x2 = " << x2;
-  
