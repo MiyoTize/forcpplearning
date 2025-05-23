f93df30f04d08b82a29c32710d5c1f14039e3d12
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-05-23 21:38:40 +0300

    Update calcoper.cpp

--- calcoper.cpp
+++ calcoper.cpp
@@ -3,7 +3,7 @@
 int calc() {
   int x;
   int y;
-  std::cout << "\nPlease, write here x and y:";
+  std::cout << "\nPlease, write here x and y:\n";
   std::cin >> x >> y;
 
   std::cout << "\nResult(+): " << x+y
@@ -11,8 +11,7 @@ int calc() {
             << "\nResult(*): " << x*y;
   if(x!=0 && y!=0) {
     std::cout << "\nResult(/): " << x/y << " " << y/x;
-  }
-  else {
+  } else {
     std::cout << "0 is evil";
   }
   return 0;
@@ -21,12 +20,12 @@ int calc() {
 int main() {
   int x, y;
   std::cout << "Heyo! Here Miyo!\n"
-            << "Do you want use calculator? (Y/n): ";
-  char a; // answer
+            << "Do you want use calculator? (1/0): ";
+  bool a; // answer
   std::cin >> a;
-  while(a == 'y') {
+  while(a == true) {
     int z = calc();
-    std::cout << "Do you want continue? (Y/n): ";
+    std::cout << "\nDo you want continue? (1/0): ";
     std::cin >> a;
   }
   std::cout << "Bye!";
