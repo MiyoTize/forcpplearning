ace436f704ca60941041c6ee06bf31a05b515cf3
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-06-11 00:22:21 +0300

    Update calcoper.cpp

--- calcoper.cpp
+++ calcoper.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
+#include <cstdint>
 
-void calc() {
-  int x;
-  int y;
+void calc(uint32_t x, uint32_t y)
+{
   std::cout << "\nPlease, write here x and y:\n";
   std::cin >> x >> y;
-
-  std::cout << "\nResult(+): " << x+y
-            << "\nResult(-): " << x-y << " " << y-x
-            << "\nResult(*): " << x*y;
+  std::cout << "\nResult(+): " << x + y
+/*MiyoTize*/<< "\nResult(-): " << x - y << " " << y-x
+/*kislyyyy*/<< "\nResult(*): " << x * y;
   if(x!=0 && y!=0) {
-    std::cout << "\nResult(/): " << x/y << " " << y/x;
+    std::cout << "\nResult(/): " << x / y << " " << y/x;
   } else {
     std::cout << "0 is evil";
   }
 }
 
-int main() {
-  int x, y;
-  std::cout << "Heyo! Here Miyo!\n"
-            << "Do you want use calculator? (Y/n): ";
-  char a; // answer
-  std::cin >> a;
+void assemblerfun(char a, uint32_t x, uint32_t y)
+{
   while(a == 'y') {
-    int z = calc();
+    calc(x, y);
     std::cout << "\nDo you want continue? (Y/n): ";
     std::cin >> a;
   }
-  std::cout << "Bye!";
+}
+
+int main()
+{
+  uint32_t x, y;
+  std::cout << "\nHeyo! Here Miyo!"
+/*sauerkat*/<< "\nDo you want use calculator?"
+            << "(Y/n):\t";
+  char a; // answer
+  std::cin >> a;
+  assemblerfun(a, x, y);
+  std::cout << "Bye!\n";
   return 0;
 }
