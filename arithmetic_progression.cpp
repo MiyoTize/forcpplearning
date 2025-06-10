ecd84990c60d1ecec69a7f0a9b9d5b3453be125c
Миё Кислющий <Sauerkrautsuppeauskater@gmail.com>
2025-06-10 22:13:26 +0300

    Update arithmetic_progression.cpp
    second shitty variant added

--- arithmetic_progression.cpp
+++ arithmetic_progression.cpp
@@ -79,3 +79,41 @@ int main()
   std::cout << "\nBye!\n\n";
   return 0;
 }
+
+
+////////////////////////////////// PLUS 
+#include <iostream>
+#include <string>
+
+class Student
+{
+  private:
+    std::string mail;
+    std::string password;
+  public:
+    std::string name;
+    unsigned age;
+    std::string gender;
+    void print() {
+      std::cout << "\nName: "   << name
+                << "\nAge: "    << age
+                << "\nGender: " << gender << std::endl;
+    }
+};
+
+int main()
+{
+  Student student;
+  Student *ptr = &student;
+  std::cout << "\nHeyo! MiyoTize Kislyy here!"
+            << "\nDo i right? You a student?\n(Y/n): ";
+  char answer;
+  std::cin >> answer;
+  if(answer == 'y') {
+    std::cout << "Write your name, age and gender\nhere: ";
+    std::cin >> student.name >> student.age >> student.gender;
+    ptr->print();
+  }
+  std::cout << "\n\nBye!\n\n";
+  return 0;
+}
