4d5d876520afe86e916f3644227963ee10f896b5
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-06-11 12:53:26 +0300

    small fix

--- fibnumbers.cpp
+++ fibnumbers.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cstdint>
 
-int fib(int n) {
-  int arr[n]{0,1};
-  for(int i=2; i<=n; i++) {
+uint32_t fib(uint32_t n)
+{
+  uint32_t arr[n]{0,1};
+  for(uint32_t i=2; i<=n; ++i) {
     arr[i] = arr[i-1] + arr[i-2];
   }
   std::cout << arr[n];
@@ -10,18 +12,21 @@ int fib(int n) {
 }
 
 int main() {
-  std::cout << "Heyo! Here Miyo!\n"
-            << "Do you want fibnum? (Y/n): ";
+  std::cout << "\nHeyo! Here Miyo!"
+/*MiyoTize*/<< "\nDo you want fibnum?"
+            << "(Y/n):\t";
   char a;
-  std::cin >> a; // asnwer
+  std::cin >> a; // answer
     
-  int n = 1; // number of fib
-  while(a == 'y') {
+  uint32_t n = 1; // number of fib
+  while(a == 'y')
+  {
     fib(n);
     ++n;
-    std::cout << "\nDo you want continue? (Y/n): ";
+    std::cout << "\nDo you want continue?"
+              << "\n(Y/n):\t";
     std::cin >> a;
   }
-  std::cout << "Bye!";
+  std::cout << "\n\nBye!\n\n";
   return 0;
 }
