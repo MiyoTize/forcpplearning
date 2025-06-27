f79fd373029ce3ad83e6104509c3e06d551a7e8c
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-06-27 20:03:16 +0300

    i rework this later

--- classstudent.cpp
+++ classstudent.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 #include <string>
+#include <cstdint>
+/*Crazyeeee Tizeeeey*/
+using std::string;
 
 class Student
 {
-  private:
-    std::string mail;
-    std::string password;
-  public:
-    std::string name;
-    unsigned age;
-    std::string gender;
-    void print() {
-      std::cout << "\nName: "   << name
-                << "\nAge: "    << age
-                << "\nGender: " << gender << std::endl;
-    }
+private:
+  string name;
+  uint16_t age; // stupid kid
+  string gender;
+
+	string mail;
+  string password;
+
+public:
+  string pmail;     // public mail
+  string ppassword; // public password
+
+	void input() { 
+  std::cout << "\nWrite your name, age and gender\nHere: ";
+  std::cin >> name >> age >> gender;
+
+  if(gender!="male" && gender!="female") {
+    std::cout << "\n\n\nGET OUT!!!\nGET OUT!!!\nGET OUT!!!\n\n\n";
+    
+  }
+  std::cout << "\nWrite here email (without @mail.com) "
+/*MiyoTize*/<< "and password\nHere: ";
+  std::cin >> pmail >> ppassword;
+
+  mail = pmail;
+	password = ppassword;
+  pmail.clear();
+  ppassword.clear();
+	}
+
+  void print() {
+    std::cout << "\nName: "   << name << pmail << ppassword
+              << "\nAge: "    << age
+              << "\nGender: " << gender << std::endl;
+  }
 };
 
 int main()
@@ -25,11 +51,12 @@ int main()
             << "\nDo i right? You a student?\n(Y/n): ";
   char answer;
   std::cin >> answer;
-  if(answer == 'y') {
-    std::cout << "Write your name, age and gender\nhere: ";
-    std::cin >> student.name >> student.age >> student.gender;
+  while(answer == 'y') {
+    ptr->input();
     ptr->print();
-  }
+    std::cout << "Do you want continue?\n(Y/n):\t";
+    std::cin >> answer;
+	}
   std::cout << "\n\nBye!\n\n";
   return 0;
 }
