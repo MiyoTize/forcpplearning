a3a3a980d24d9aa1fc1047945319470eb7ca7158
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-07-26 01:32:50 +0300

    for someone see

--- classstudent.cpp
+++ classstudent.cpp
@@ -2,61 +2,67 @@
 #include <string>
 #include <cstdint>
 /*Crazyeeee Tizeeeey*/
-using std::string;
+using std::cout, std::cin, std::endl, std::string;
 
 class Student
 {
 private:
   string name;
-  uint16_t age; // stupid kid
-  string gender;
-
+  uint16_t age = {}; // stupid kid
 	string mail;
   string password;
+  uint8_t grade;
+
+  enum gender { // only 2 !!!
+    male,
+    female
+  };
 
 public:
-  string pmail;     // public mail
-  string ppassword; // public password
-
-	void input() { 
-  std::cout << "\nWrite your name, age and gender\nHere: ";
-  std::cin >> name >> age >> gender;
-
-  if(gender!="male" && gender!="female") {
-    std::cout << "\n\n\nGET OUT!!!\nGET OUT!!!\nGET OUT!!!\n\n\n";
-    
+  string GetСredentials() {
+    return mail;
+    return password;
   }
-  std::cout << "\nWrite here email (without @mail.com) "
-/*MiyoTize*/<< "and password\nHere: ";
-  std::cin >> pmail >> ppassword;
+  void SetCredentials() {
+    cin >> mail;
+    cin >> password;
+  };
+	void input() {
+    cout << "\nWrite your name, age, gender and grade\nHere: ";
+    cin >> name >> age >> gender;
+  };
 
-  mail = pmail;
-	password = ppassword;
-  pmail.clear();
-  ppassword.clear();
-	}
-
+  while(gender!="male" && gender!="female") {
+    cout << "\n\n\nGET OUT!!!\nGET OUT!!!\nGET OUT!!!\n\n\n";
+    cout << "Write gender here, asshole!: ";
+    cin >> gender;
+  }
+  cout << "\nWrite here email (without @mail.comG): ";
+  SetCredentials();
+  
   void print() {
-    std::cout << "\nName: "   << name << pmail << ppassword
-              << "\nAge: "    << age
-              << "\nGender: " << gender << std::endl;
+    cout << "\nName: "   << name
+/*MiyoThe*/<< "\nAge: "    << age
+/*Crazyee*/<< "\nGender: " << gender << endl;
   }
 };
 
 int main()
 {
-  Student student;
-  Student *ptr = &student;
-  std::cout << "\nHeyo! MiyoTize Kislyy here!"
-            << "\nDo i right? You a student?\n(Y/n): ";
-  char answer;
-  std::cin >> answer;
-  while(answer == 'y') {
-    ptr->input();
-    ptr->print();
-    std::cout << "Do you want continue?\n(Y/n):\t";
-    std::cin >> answer;
-	}
-  std::cout << "\n\nBye!\n\n";
+  Student person;
+  cout << "\nHeyo! MiyoTize Kislyy here!"
+/*Hi!*/<< "\nYou a student/teacher?\n";
+  string answer; // student or teacher
+  cin >> answer;
+/*Too much lines. Get chill, reader! This code doesnt work!!! Vlad is angry! Br-rr! RuWust is my fav >_<*/
+  if(answer == "teacher") {
+    person.print();
+	} else if(answer == "student") {
+    person.input();
+  } else {
+    cout << "\nTry again!\n";
+    return 0;
+  }
+  cout << "\n\nBye!\n\n";
   return 0;
-}
+} // Добавить функцию "ОТЧИСЛЕН!!!"
