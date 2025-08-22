0c6d98180a031567cb935b787bd22f297289b0cd
MiyoTize <sauerkrautsuppeauskater@gmail.com>
2025-08-22 20:07:52 +0300

    i will reinstall repo

---
+++ not-completed/hanoi-tower.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <random>
+using std::cout, std::cin;
+
+class hanoi {
+uint32_t width; // how much towers we have 
+uint32_t height; // how much size of tower (layers)
+
+public:
+  uint32_t GetW {
+    return width;
+  }
+  void SetW {
+    cin >> width;
+  }
+
+  uint32_t GetH {
+    return height;
+  }
+  void SetH {
+    cin >> height;
+  }
+
+  
+
+};
+
+
+int main() {
+
+  return 0;
+}
